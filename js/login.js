3fd05a54cb619fedb2ef523b25af99b1fcd5383c
morayam <110411288+morayam@users.noreply.github.com>
2022-09-02 12:10:41 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -18,7 +18,7 @@ function loginUser() {
   console.log(usersl)
   location.reload(location.href ='index.html')
 }*/
-.
+/*.*/
 /*function userRegistration() {
     const userData = {
         document.getElementById('register-email').value,
